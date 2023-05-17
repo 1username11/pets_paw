f83eaeecfcd0a090b616c8fddf22e09fad44d83c
aider <auder31101997@gmail.com>
2023-05-17 13:44:23 +0300

    dynamic routes removed

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,51 +1,60 @@
 import type { RouteRecordRaw } from 'vue-router'
 import { routeNames } from '@/router/route-names'
+import Home from '@/views/home-page/Home.vue'
+import Breeds from '@/views/breeds/Breeds.vue'
+import BreedsInfo from '@/views/breeds/components/BreedsInfo.vue'
+import Gallery from '@/views/gallery/Gallery.vue'
+import Voting from '@/views/voting/Voting.vue'
+import Likes from '@/views/likes/Likes.vue'
+import Dislikes from '@/views/dislikes/Dislikes.vue'
+import Favorite from '@/views/favorite/Favorite.vue'
+import Search from '@/views/search/Search.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: routeNames.home,
-    component: import('@/views/home-page/Home.vue')
+    component: Home
   },
   {
     path: '/breeds',
     name: routeNames.breeds,
-    component: import('@/views/breeds/Breeds.vue')
+    component: Breeds
   },
   {
     path: '/gallery',
     name: routeNames.gallery,
-    component: import('@/views/gallery/Gallery.vue')
+    component: Gallery
   },
   {
     path: '/voting',
     name: routeNames.voting,
-    component: import('@/views/voting/Voting.vue')
+    component: Voting
   },
   {
     path: '/likes',
     name: routeNames.likes,
-    component: import('@/views/likes/Likes.vue')
+    component: Likes
   },
   {
     path: '/dislikes',
     name: routeNames.dislikes,
-    component: import('@/views/dislikes/Dislikes.vue')
+    component: Dislikes
   },
   {
     path: '/favorite',
     name: routeNames.favorite,
-    component: import('@/views/favorite/Favorite.vue')
+    component: Favorite
   },
   {
     path: '/breeds/:id',
     name: routeNames.breedsInfo,
-    component: import('@/views/breeds/components/BreedsInfo.vue')
+    component: BreedsInfo
   },
   {
     path: '/search',
     name: routeNames.search,
-    component: import('@/views/search/Search.vue')
+    component: Search
   },
   {
     path: '/:pathMatch(.*)*',
